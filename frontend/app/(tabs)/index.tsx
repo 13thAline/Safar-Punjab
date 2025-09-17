6109aff82a0ad7098009fa7ed09b0ac8e206699a
Nipuna <nipunamahakur@gmail.com>
2025-09-17 16:48:24 +0530

    Testing native

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -4,8 +4,8 @@ import "../global.css"
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-200">
-      <Text className="text-white text-2xl font-bold">Tailwind Works ✅</Text>
+    <View className="flex-1 items-center justify-center bg-yellow-100">
+      <Text className="text-black text-2xl font-bold">Tailwind Works</Text>
     </View>
   );
 }
