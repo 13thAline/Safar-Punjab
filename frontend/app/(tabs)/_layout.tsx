ac604c45d8e57fb7829c683c62e6f47918baf9c1
fomo-ash <ashutoshbadapanda02@gmail.com>
2025-09-20 19:49:07 +0530

    small rectification in layout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 import { Stack } from "expo-router";
 
-=======
-// app/_layout.tsx
-// import DriverVerificationScreen from '../driver-verification';
-
-// export default function Index() {
-//   return <DriverVerificationScreen />;
-// }
-
-import { Stack } from "expo-router";
-
->>>>>>> 0f526719aeffc36a9d54e515a0fccc83c7026e1a
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
